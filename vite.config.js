7987beb9e153981a5a633cd1108d4aac50dd2ef5
onsyedes <ons.yedes@tek-up.de>
2025-11-20 18:38:24 +0100

    --

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import { ghPages } from "vite-plugin-gh-pages";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), ghPages()],
+  base : "/portfolio",
 });
